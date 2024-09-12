b2109d975943911e42426f5c231782517791651d
Eva.Kusyakina <eva.kusyakina@gmail.com>
2024-09-12 07:47:57 +0300

    Change code second

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        int InitialBalance = 50; //начальный баланс
-        int Deposit = 1100; //сумма пополнения
-        int Bonus; // бонус
-        int FinalBalance; //конечный баланс
+        int initialbalance = 50; //начальный баланс
+        int deposit = 1100; //сумма пополнения
+        int bonus; // бонус
+        int finalbalance; //конечный баланс
 
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
 
-        if (Deposit > 1000) {
-            Bonus = Deposit / 100;
+        if (deposit > 1000) {
+            bonus = deposit / 100;
         } else {
-            Bonus = 0;
+            bonus = 0;
         }
-        FinalBalance = InitialBalance + Deposit + Bonus;
+        finalbalance = initialbalance + deposit + bonus;
 
         System.out.println("Количество начисленных бонусов:");
-        System.out.println(Bonus + " миль");
+        System.out.println(bonus + " миль");
         System.out.println("Конечный баланс счета:");
-        System.out.println(FinalBalance + " рублей");
+        System.out.println(finalbalance + " рублей");
 
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
